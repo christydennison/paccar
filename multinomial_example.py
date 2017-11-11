82bafce2acbcda4e1baa1b8dda60ec1158b02653
Julio A Martinez <julioamz@users.noreply.github.com>
2017-11-11 13:52:22 -0800

    Update multinomial_example.py

--- multinomial_example.py
+++ multinomial_example.py
@@ -31,3 +31,45 @@ model = model.fit(bob_says)
 logprob, alice_hears = model.decode(bob_says, algorithm="viterbi")
 print "Bob says:", ", ".join(map(lambda x: observations[x], bob_says))
 print "Alice hears:", ", ".join(map(lambda x: states[x], alice_hears))
+
+
+
+
+
+
+
+from __future__ import division
+import numpy as np
+from hmmlearn import hmm
+
+states = ["Healthy", "Critical", "Broken"]
+n_states = len(states)
+
+observations = ["cold", "hot"]
+n_observations = len(observations)
+
+start_probability = np.array([0.6, 0.4])
+
+transition_probability = np.array([
+  [0.6, 0.3, 0,1],
+  [0.3, 0.4, 0.3]
+  [0, 0, 1]
+])
+
+emission_probability = np.array([
+  [0.8, 0.2],
+  [0.5, 0.5]
+  [0.1, 0.9]
+])
+
+model = hmm.MultinomialHMM(n_components=n_states)
+model.startprob_=start_probability
+model.transmat_=transition_probability
+model.emissionprob_=emission_probability
+
+# predict a sequence of hidden states based on visible states
+conditions = [0, 1, 1, 1, 1, 1]
+model = model.fit(conditions)
+logprob, results = model.decode(conditions, algorithm="viterbi")
+print "Bob says:", ", ".join(map(lambda x: observations[x], conditions))
+print "Alice hears:", ", ".join(map(lambda x: states[x], results))
