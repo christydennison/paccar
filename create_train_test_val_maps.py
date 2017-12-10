acb050745facbd52286cfd7e8701d6190d6f66af
Julio Martinez <martnzjulioa@gmail.com>
2017-12-10 21:58:43 +0000

    this has the loads function

--- create_train_test_val_maps.py
+++ create_train_test_val_maps.py
@@ -6,6 +6,7 @@ s_veh_key = 'Veh Ref ID'
 r_veh_key = 'Chassis Reference Number'
 s_time_key = 'Event DateTime'
 r_time_key = 'Rpr_Dt'
+code = 'ATA6'
 
 def get_slices(num_windows, window_size, snapshots, ignore_past=False):
     slices = []
@@ -25,8 +26,8 @@ def get_slices(num_windows, window_size, snapshots, ignore_past=False):
     return slices
 
 def get_repair_slices_map(veh_ids, snapshots, repairs, num_windows=10, window_size=10, code='ATA9', ignore_past=False):
-    repairs = repairs.drop(['Unnamed: 0'],1)
-    snapshots = snapshots.drop(['Unnamed: 0'],1)
+    repairs = repairs
+    snapshots = snapshots
     repair_slices = {}
     for veh_id in veh_ids:
         v_snapshots = snapshots[snapshots[s_veh_key] == veh_id].sort_values(by=s_time_key)
@@ -56,18 +57,26 @@ def get_repair_slices_map(veh_ids, snapshots, repairs, num_windows=10, window_si
             for end in repair_group[r_time_key]:
                 range_mask = (v_snapshots[s_time_key] >= start) & (v_snapshots[s_time_key] <= end)
                 snapshot_range = v_snapshots[range_mask]
-                
+
                 for i,slices in get_slices(num_windows, window_size, snapshot_range, ignore_past):
                     if len(slices) > 0:
                         if i not in veh_slices_repair:
                             veh_slices_repair[i] = []
-                        veh_slices_repair[i].append(slices)
+                        veh_slices_repair[i].append(slices.drop(['Veh Ref ID','Event DateTime'],1))
 
                 ## reset start to end for next iteration
                 start = end
 
     return repair_slices
 
+def get_ok_slices_map(veh_ids, snapshots, num_windows, window_size):
+    ok_map = {}
+    for veh_id in veh_ids:
+        ok_map[veh_id] = {0:{0:[]}}
+        v_snapshots = snapshots[snapshots[s_veh_key] == veh_id].sort_values(by=s_time_key)
+        for i,slices in get_slices(num_windows, window_size, v_snapshots, True):
+            ok_map[veh_id][0][0].append(slices.drop(['Veh Ref ID','Event DateTime'],1))
+    return ok_map
 
 def save_map(repairs_map, filename):
     for veh_id in repairs_map.keys():
@@ -85,9 +94,7 @@ def open_map(filename):
             veh_id = int(keys[1])
             code = int(keys[2])
             window = int(keys[3])
-            i = int(keys[4])
-            n = "{}_{}_{}_{}_{}_.pkl".format(filename, veh_id, code, window, i)            
-            repairs_map_slice = pandas.read_pickle(n)
+            repairs_map_slice = pandas.read_pickle(file)
             if veh_id not in repairs_map:
                 repairs_map[veh_id] = {}
             if code not in repairs_map[veh_id]:
@@ -97,3 +104,63 @@ def open_map(filename):
             repairs_map[veh_id][code][window].append(repairs_map_slice)
 
     return repairs_map
+
+def save_ok_map(ok_map, filename):
+    for veh_id in ok_map.keys():
+        slices = ok_map[veh_id][0][0]
+        for i,s in enumerate(slices):
+            n = "{}_OK_{}_{}_.pkl".format(filename, veh_id, i)
+            s.to_pickle(n)
+
+def open_ok_map(filename):
+    ok_map = {}
+    for file in glob.glob("{}*".format(filename)):
+        keys = file.split("_")
+        if len(keys) == 5 and keys[1] == "OK":
+            veh_id = int(keys[2])
+            if veh_id not in ok_map:
+                ok_map[veh_id] = {0:{0:[]}}
+            ok_map_slice = pandas.read_pickle(file)
+            ok_map[veh_id][0][0].append(ok_map_slice)
+
+    return ok_map
+
+def create_maps(num_windows, window_size, snapshots, ok_snapshots, ids_tuple, ids_ok_tuple, repairs_tuple):
+    train_ids, val_ids, test_ids = ids_tuple
+    train_ok_ids, val_ok_ids, test_ok_ids = ids_ok_tuple
+    train_repairs, val_repairs, test_repairs = repairs_tuple
+    
+    train_repairs_map = get_repair_slices_map(train_ids, snapshots, train_repairs, num_windows=num_windows, window_size=window_size, code=code, ignore_past=True)
+    val_repairs_map = get_repair_slices_map(val_ids, snapshots, val_repairs, num_windows=num_windows, window_size=window_size, code=code, ignore_past=True)
+    test_repairs_map = get_repair_slices_map(test_ids, snapshots, test_repairs, num_windows=num_windows, window_size=window_size, code=code, ignore_past=True)
+    
+    train_ok_map = get_ok_slices_map(train_ok_ids, ok_snapshots, num_windows, window_size)
+    val_ok_map = get_ok_slices_map(val_ok_ids, ok_snapshots, num_windows, window_size)
+    test_ok_map = get_ok_slices_map(test_ok_ids, ok_snapshots, num_windows, window_size)
+
+    name = "{}-{}".format(num_windows, window_size)
+    
+    save_map(train_repairs_map, '/home/cs231n/data/train-'+name)
+    save_map(val_repairs_map, '/home/cs231n/data/val-'+name)
+    save_map(test_repairs_map, '/home/cs231n/data/test-'+name)
+    
+    save_ok_map(train_ok_map, '/home/cs231n/data/trainOK-'+name)
+    save_ok_map(val_ok_map, '/home/cs231n/data/valOK-'+name)
+    save_ok_map(test_ok_map, '/home/cs231n/data/testOK-'+name)
+
+def load_maps(num_windows, window_size):
+    name = "{}-{}".format(num_windows, window_size)
+    
+    train_map_deser = open_map('/home/cs231n/data/train-'+name)
+    val_map_deser = open_map('/home/cs231n/data/val-'+name)
+    test_map_deser = open_map('/home/cs231n/data/test-'+name)
+
+    train_map_deser_ok = open_ok_map('/home/cs231n/data/trainOK-'+name)
+    val_map_deser_ok = open_ok_map('/home/cs231n/data/valOK-'+name)
+    test_map_deser_ok = open_ok_map('/home/cs231n/data/testOK-'+name)
+    
+    train_combined = {**train_map_deser, **train_map_deser_ok}
+    val_combined = {**val_map_deser, **val_map_deser_ok}
+    test_combined = {**test_map_deser, **test_map_deser_ok}
+    
+    return (train_combined, val_combined, test_combined)
